09a20a9c4355c708ef2bb0d648de904fe4894cdd
ROHIT <rohitgangurde@u.boisestate.edu>
2020-02-28 20:52:41 -0700

    Added dictionary support

--- Proj1/fa/dfa/DFAState.java
+++ Proj1/fa/dfa/DFAState.java
@@ -3,65 +3,89 @@ import java.util.* ;
 
 public class DFAState extends fa.State {
 
-    protected String name;
-    private Set<DFAState> nextStates;
-    private char nextTransition;
-    private Boolean isFinal;
-    private Boolean isStart;
+	protected String name;
+	private Set<DFAState> nextStates;
+    	private Dictionary nextStateWithTransition;
+	private char nextTransition;
+	private Boolean isFinal;
+	private Boolean isStart;
 
-    public DFAState(String name){
-        this.name = name;
-        this.nextStates = new HashSet<DFAState>();
-        this.nextTransition = 'n';
-        this.isFinal = false;
-        this.isStart = false;
-    }
-    /**
-     * Sets the states after transition
-     * @param nextState
-     */
-    public void setNext(Set<DFAState> nextStates){ this.nextStates = nextStates; }
+    	public DFAState(String name){
+        	this.name = name;
+        	this.nextStates = new HashSet<DFAState>();
+		this.nextStateWithTransition = new HashTable();
+        	this.nextTransition = 'n';
+        	this.isFinal = false;
+        	this.isStart = false;
+    	}
+    	/**
+     	* Sets the states after transition
+     	* @param nextState
+     	*/
+    	public void setNext(Set<DFAState> nextStates){ this.nextStates = nextStates; }
 
-    /**
-     * Returns the state after transition
-     * @return nextState : State object
-     */
-    public DFAState getNext(String transition){
-        DFAState ret = null;
-        for(Iterator<DFAState> it = nextStates.iterator(); it.hasNext();){
-            DFAState nextOne = it.next();
-            ret = nextOne;
-            if (transition == nextOne.getTransition()) break;
-        }
-        return ret;
-    }
+    	/**
+     	* Returns the state after transition
+     	* @return nextState : State object
+     	*/
+    	public DFAState getNext(String transition){
+        	DFAState ret = null;
+        	for(Iterator<DFAState> it = nextStates.iterator(); it.hasNext();){
+            		DFAState nextOne = it.next();
+            		ret = nextOne;
+            		if (transition == nextOne.getTransition()) break;
+        	}
+        	return ret;
+    	}
 
-    /**
-     * Sets the transition of the current state
-     * @param transition
-     */
-    public void setTransition(char transition){ nextTransition = transition; }
+	/**
+ 	* Returns the next state given the transition
+ 	* @param transition : char representing the transition
+ 	* @return ret : next state if found, -1 otherwise
+ 	*/ 
+	public DFAState getNextWithTransition(char transition){
+		DFAState ret = nextStateWithTransition.get(transition);
+		if(ret.equals(null)) return -1;
+		else return ret;
+	}
+	
+	/**
+ 	*Adds the next state to the nextStateWithTransition Dictionary
+ 	* @param state : DFAState object to add with the corresponding transition
+ 	* @return transition : char representing the transition
+ 	*/  
+	public void addStateWithTransition(DFAState state, char transition){
+		if(nextStateWithTransition.get(transition).equals(null))
+			this.nextStateWithTransition.put(transition, state);
+		else return -1;
+	}
+    	
+	/**
+     	* Sets the transition of the current state
+     	* @param transition
+     	*/
+    	public void setTransition(char transition){ nextTransition = transition; }
 
-    /**
-     * Gets the transition string of the current state
-     * @return nextTransition
-     */
-    public char getTransition(){ return nextTransition; }
+    	/**
+     	* Gets the transition string of the current state
+     	* @return nextTransition
+     	*/
+    	public char getTransition(){ return nextTransition; }
 
-    /**
-     * Sets the current state as the final state
-     */
-    public void setFinalState(){ isFinal = true; }
+    	/**
+     	* Sets the current state as the final state
+     	*/
+    	public void setFinalState(){ isFinal = true; }
 
-    /**
-     * Sets the current state as the start state
-     */
-    public void setStartState(){ isStart = true; }
+    	/**
+     	* Sets the current state as the start state
+     	*/
+    	public void setStartState(){ isStart = true; }
 
-    public String getName(){ return name; }
-    /**
-     * Returns a string representation of the state
-     * @return
-     */
-    public String toString(){ return name; }
+    	public String getName(){ return name; }
+    	/**
+     	* Returns a string representation of the state
+     	* @return
+     	*/
+    	public String toString(){ return name; }
 }
